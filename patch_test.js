991c992bb613be608f74d45cda92f82f464b8720
Prabhav Jani <31888813+progressivePRV@users.noreply.github.com>
2021-03-05 20:53:50 -0500

    modified patch code js file

--- patch_test.js
+++ patch_test.js
@@ -83,19 +83,31 @@ app.patch("/api/v1/company",san_val_patch_company, (req, res) => {
         .getConnection()
         .then((conn) => {
             // let query = "select * from company where COMPANY_ID=" + req.params.id.trim();
-            let query = "select * from company where COMPANY_ID=" + "10";
+            let query = "select * from company where COMPANY_ID=" + req.body.COMPANY_ID;
             conn
                 .query(query)
                 .then((rows) => {
                     conn.release();
-                    let output = {}
-                    if (rows.affectedRows > 0) {
-                        output.msg = "Successful";
-                    } else {
-                        output.msg = "No change";
-                    }
-                    output.database_output = rows;
-                    res.json(output);
+                    // id exists
+                    conn.query(query_update)
+                        .then((rows) => {
+                            conn.release();
+                            let output = {}
+                            if (rows.affectedRows > 0) {
+                                output.msg = "Successful";
+                            } else {
+                                output.msg = "No change";
+                            }
+                            output.database_output = rows;
+                            res.json(output);
+                        })
+                        .catch((err) => {
+                            conn.release();
+                            let output = {}
+                            output.msg = 'for more information goto URL=>"https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-reference-error-sqlstates.html#:~:text=mysql%20error%20number%20mysql%20error%20name%20sql%20standard,er_no_db_error%3A%203d000%3A%201047%3A%20er_unknown_com_error%3A%2008s01%3A%201048%3A%20er_bad_null_error%3A%2023000"'
+                            output.database_output = err;
+                            res.json({ errors: output });
+                        });
                 })
                 .catch((err) => {
                     conn.release();
