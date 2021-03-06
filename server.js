58be131ef3ffc622f114f045fda6bd34f08f7c68
Prabhav Jani <31888813+progressivePRV@users.noreply.github.com>
2021-03-05 19:50:02 -0500

    modifed response for  api

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ app.post("/api/v1/company", san_val_post_company, (req, res) => {
             let output = {}
             output.msg = 'for more information goto URL=>"https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-reference-error-sqlstates.html#:~:text=mysql%20error%20number%20mysql%20error%20name%20sql%20standard,er_no_db_error%3A%203d000%3A%201047%3A%20er_unknown_com_error%3A%2008s01%3A%201048%3A%20er_bad_null_error%3A%2023000"'
             output.database_output = err;
-            res.json(output);
+            res.json({errors:output});
           });
     })
     .catch((err) => {
@@ -309,7 +309,7 @@ app.patch("/api/v1/company", san_val_patch_company, (req, res) => {
                 let output = {}
                 output.msg = 'for more information goto URL=>"https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-reference-error-sqlstates.html#:~:text=mysql%20error%20number%20mysql%20error%20name%20sql%20standard,er_no_db_error%3A%203d000%3A%201047%3A%20er_unknown_com_error%3A%2008s01%3A%201048%3A%20er_bad_null_error%3A%2023000"'
                 output.database_output = err;
-                res.json(output);
+                res.json({errors:output});
               });
             // as already above query will send the response
             // conn.release();
@@ -320,7 +320,7 @@ app.patch("/api/v1/company", san_val_patch_company, (req, res) => {
             let output = {}
             output.msg = 'for more information goto URL=>"https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-reference-error-sqlstates.html#:~:text=mysql%20error%20number%20mysql%20error%20name%20sql%20standard,er_no_db_error%3A%203d000%3A%201047%3A%20er_unknown_com_error%3A%2008s01%3A%201048%3A%20er_bad_null_error%3A%2023000"'
             output.database_output = err;
-            res.json(output);
+            res.json({errors:output});
           });
       })
       .catch((err) => {
@@ -391,7 +391,7 @@ app.delete("/api/v1/company/:id", san_val_del_company, (req, res) => {
           let output = {}
           output.msg = 'for more information goto URL=>"https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-reference-error-sqlstates.html#:~:text=mysql%20error%20number%20mysql%20error%20name%20sql%20standard,er_no_db_error%3A%203d000%3A%201047%3A%20er_unknown_com_error%3A%2008s01%3A%201048%3A%20er_bad_null_error%3A%2023000"'
           output.database_output = err;
-          res.json(output);
+          res.json({errors:output});
         });
     })
     .catch((err) => {
